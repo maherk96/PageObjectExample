374e82245a7d6909d237ef1458c8dc429243255d
Maher <maherkarim96@gmail.com>
2020-12-09 06:41:07 +0000

    Added logging and quantity checks

--- src/test/java/Helper/BaseScript.java
+++ src/test/java/Helper/BaseScript.java
@@ -1,6 +1,5 @@
 package Helper;
 
-
 import Pages.ArgosHomePage;
 import Pages.ItemsPage;
 import org.openqa.selenium.WebDriver;
@@ -8,31 +7,40 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.BeforeTest;
 import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import static Pages.ArgosHomePage.acceptCookies;
 
 public class BaseScript {
 
+
+
     protected WebDriver driver;
     protected ArgosHomePage argosHomePage;
     protected ItemsPage itemsPage;
-
     public static String baseUrl = "https://www.argos.co.uk/";
-
+    private static final Logger logger = Logger.getLogger(BaseScript.class.getName());
 
     public void acceptCookies() {
         acceptCookies.click();
     }
 
-    @BeforeSuite
-    public void doBeforeSuite() {
-        //System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
-        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/main/resources/drivers/mac/chrome/chromedriver");
-
-
+    public static Integer checkQuantityLevel(int quantity) {
+        if (quantity < 1 || quantity > 10) {
+            logger.log(Level.WARNING, "Order Quantity 1 > 10, Enter valid Quantity");
+            throw new UnsupportedOperationException();
+        } else {
+            return quantity;
+        }
     }
 
+    @BeforeSuite
+    public void doBeforeSuite() {
+        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+        //System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/main/resources/drivers/mac/chrome/chromedriver");
+    }
 
     @BeforeMethod
     public void doBeforeTest() {
